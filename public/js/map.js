84f2ff5d09f42d04ad51adf2ff57b1ed9d7a743e
Mark Timbol <mark.timbol@hotmail.com>
2015-10-26 16:54:41 +0400

    Fixed background image

--- public/js/map.js
+++ public/js/map.js
@@ -1,1 +1,12 @@
-function initialize(){var e=document.getElementById("map"),o={center:new google.maps.LatLng(25.099341,55.177762),zoom:16,mapTypeId:google.maps.MapTypeId.ROADMAP};new google.maps.Map(e,o)}google.maps.event.addDomListener(window,"load",initialize);
+function initialize() {
+	var mapCanvas = document.getElementById('map');
+	var mapOptions = {
+		center: new google.maps.LatLng(25.099341, 55.177762),
+		zoom: 16,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+//# sourceMappingURL=map.js.map
